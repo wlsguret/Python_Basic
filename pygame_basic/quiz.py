8a13d7001de8697a500453df35802869da24eb72
Doritos <83631924+wlsguret@users.noreply.github.com>
2021-08-15 15:47:13 +0900

    운석피하기 게임 title 변경 py game --> 운석피하기

--- pygame_basic/quiz.py
+++ pygame_basic/quiz.py
@@ -10,7 +10,7 @@ screen_height = 640 # 세로 크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # 화면 타이틀 설정
-pygame.display.set_caption("Quiz Game") 
+pygame.display.set_caption("운석 피하기") 
 
 # background = pygame.image.load("C:\\Users\\USER\\Desktop\\PythonWorkspace\\pygame_basic\\background.png")
 background = pygame.image.load("C:\\Users\\USER\\Desktop\\VSCode workspace\\Python\\pygame_basic\\background.png")
